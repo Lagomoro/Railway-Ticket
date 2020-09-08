87d16b13389c4c8412da533ea81ce99a9c4537ef
Lagomoro <2669708373@qq.com>
2020-09-08 15:14:22 +0800

    Initial Commit

---
+++ src/test/java/pers/lagomoro/railwayticket/RailwayTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.lagomoro.railwayticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

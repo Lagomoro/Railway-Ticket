810753778bdf7b8c1a7afc9aa30f7ceeb142c935
Lagomoro <2669708373@qq.com>
2020-09-11 09:22:13 +0800

    2020-9-11

--- src/main/java/pers/lagomoro/railway_system/RailwayTicketApplication.java
+++ src/main/java/pers/lagomoro/railway_system/RailwayTicketApplication.java
@@ -1,10 +1,13 @@
-package pers.lagomoro.railway_system.runnable;
+package pers.lagomoro.railway_system;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication(scanBasePackages="pers.lagomoro.railway_system",
+        exclude = DruidDataSourceAutoConfigure.class)
+@MapperScan("pers.lagomoro.railway_system.mapper")
 public class RailwayTicketApplication {
 
     public static void main(String[] args) {
--- src/main/java/pers/lagomoro/railway_system/controller/HelloController.java
+++ src/main/java/pers/lagomoro/railway_system/controller/HelloController.java
@@ -1,14 +1,44 @@
 package pers.lagomoro.railway_system.controller;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import pers.lagomoro.railway_system.entity.User;
+import pers.lagomoro.railway_system.mapper.UserMapper;
+import pers.lagomoro.railway_system.service.UserService;
+import pers.lagomoro.railway_system.service.impl.UserServiceImpl;
 
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.ServletContext;
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    private final UserServiceImpl userService;
+
+    public HelloController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/hello")
     public String hello(){
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserMapper dao = ac.getBean(UserMapper.class);
+        User u1 = dao.selectByPrimaryKey(1);
+        System.out.println(u1.getUsername());
         return "Hello World!";
     }
+
+    @GetMapping("/user")
+    public String users1() {
+        return userService.selectByPrimaryKey(1).getUsername();
+    }
+
 }
